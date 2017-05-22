9201d0f21b859437dacc0d742a34fda378b32429
Vlad Z <vladislavz1991@gmail.com>
2017-05-22 11:08:28 -0400

    comments

--- Chapter 1 - Hash Table/Challenge 3/Solution 1.playground/Contents.swift
+++ Chapter 1 - Hash Table/Challenge 3/Solution 1.playground/Contents.swift
@@ -8,22 +8,27 @@ let test5 = "g ooodddw d"
 let test6 = "god"
 
 extension String {
+    /// Simplify char count
     public var length: Int {
         return characters.count
     }
     
+    /// If two strings are anagrams, then we know they have the same characters, but in different order
     func sort()-> String {
         let content = utf16.sorted()
         return String(describing: content)
     }
 }
 
+
 func permutation(_ string1: String, string2: String)-> Bool {
-    
+
+    /// Check if different lengths
     if string1.length != string2.length {
         return false
     }
     
+    /// Check if they are identical
     return string1.sort() == string2.sort()
 }
 
