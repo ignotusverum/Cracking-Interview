f9f5d5221d66238b7dd8095b943667d60d675563
Vlad Z <vladislavz1991@gmail.com>
2017-09-14 14:00:45 -0400

    setup

---
+++ Chapter 1 - Hash Table/Challenge 4/Solution - 1.playground/Contents.swift
@@ -0,0 +1,13 @@
+/// Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end of the stirng to hold the additional chars, and that you are given the true length of a string.
+
+let test1 = "123123123 12312312"
+let test2 = "123123123 123123 12"
+let test3 = "          123123123 123123 12"
+
+test2.characters
+
+let teset = [1,2,3,2]
+
+let result = teset.enumerated().filter { $0.element == 2 }.map { $0.offset }
+
+print(result)
